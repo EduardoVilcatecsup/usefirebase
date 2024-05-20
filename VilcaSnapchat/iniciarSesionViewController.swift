98ee1b150a139ce9af8be2abe6a1556d99dae4e6
EduardoVilca <eduardo.vilca@tecsup.edu.pe>
2024-05-20 18:43:16 -0500

    mejorado acabado

--- VilcaSnapchat/iniciarSesionViewController.swift
+++ VilcaSnapchat/iniciarSesionViewController.swift
@@ -8,8 +8,36 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 class iniciarSesionViewController: UIViewController {
 
+    
+    @IBAction func googlexdxd(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+            return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+
+          // ...
+        }
+    }
     
     @IBOutlet weak var emailTextField: UITextField!
     
